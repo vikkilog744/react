e53e53ae5875ca4eabddff628fbe45a4a039be27
screensquad <zeroscreensquad@gmail.com>
2024-12-15 19:01:10 +0530

    updated latest new react app

--- src/App.js
+++ src/App.js
@@ -1,63 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import { useEffect } from "react";
+import logo from './logo.svg';
+import './App.css';
 
-import Menu from './components/menu';
-import Dashboard from './components/dashboard';
-import About from './components/about';
-import Product from './components/product';
-import ContactUs from './components/contactUs';
-import NotFound from './notFound';
-
-import Test1 from './test/test1.js';
-import Test2 from './test/test2.js';
-import Test3 from './test/test3.js';
-
-import customData from './customData.json';
-
-const App = () => {
-  const windowLocation = window.location;
-
-  useEffect(() => {
-    return () => {
-      if (windowLocation?.hash !== '') {
-        window.location.href = '/';
-      }
-    };
-  }, [windowLocation]);
-
-  const renderMenu = () => {
-    return (
-      <Menu data={customData && customData?.menuRoutes.filter(name => name?.menuVisible)} />
-    )
-  };  
-
-  const renderSwitchComponent = (routeURL) => {
-    switch(routeURL) {
-      case '/':
-        return renderMenu();
-      case '/dashboard':
-        return (<>{renderMenu()}<Dashboard /></>);
-      case '/about':
-        return (<>{renderMenu()}<About /></>);
-      case '/product':
-        return (<>{renderMenu()}<Product /></>);
-      case '/contactUs':
-        return (<>{renderMenu()}<ContactUs /></>);
-      default:
-        return <NotFound />;
-    }
-  };
-
-  return (<>
-    <Routes>
-      {customData && customData?.menuRoutes.map((item, index) => (
-        <Route key={index} exact path={item.menuUrl} element={renderSwitchComponent(item.menuUrl)} />
-      ))}
-    </Routes>
-    <Test1>Test Excel</Test1>
-    <Test2>Test Excel</Test2>
-    <Test3>Test Excel</Test3>
-  </>);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-// import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter location={window.location.pathname} context={{}}>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+reportWebVitals();
